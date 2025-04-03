2b27f441e24cda2f8f685640d74daa08ae7c10f7
willy0483 <willyjensen251@gmail.com>
2025-04-03 13:19:47 +0200

    fix route load static when needed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,31 @@
+import { useLocation } from "react-router-dom";
 import { Main } from "./components/main/main";
 import { AppRoutes } from "./routes/appRoutes";
 
 const App = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
   return (
-    // Header
+    <>
+      {/* Header */}
+      {pathname === "/register" || pathname === "/" ? null : (
+        <header>
+          <h1>Header</h1>
+        </header>
+      )}
 
-    // Main
-    <Main>
-      <AppRoutes />
-    </Main>
+      {/* Main */}
+      <Main>
+        <AppRoutes />
+      </Main>
 
-    // Footer
+      {/* Footer */}
+      {pathname === "/register" || pathname === "/" ? null : (
+        <footer>
+          <h1>Footer</h1>
+        </footer>
+      )}
+    </>
   );
 };
 
