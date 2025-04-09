52c6e415d96822ae025405d0391d715e7a5e7487
crds83 <119341646+crds83@users.noreply.github.com>
2025-04-09 09:14:17 +0100

    homepage

--- frontend/src/pages/homePage/homePage.style.js
+++ frontend/src/pages/homePage/homePage.style.js
@@ -4,31 +4,67 @@ import styled from 'styled-components';
 export const HomePageStyle = styled.div`
  background-color: ${(props) => props.theme.color.blue};
  width: 100%;
- height: 80vh;
- padding-top: 3vh;
+ height: 250px;
+ 
+ padding-bottom: 50px;
 
 `;
 
-export const SubjectsWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-  padding: 20px;
-  background-color: ${(props) => props.theme.color.blue}; /* optional */
+export const Title = styled.h1 `
+color: #000;
+margin-top: 31px;
+margin-bottom: 16px;
+margin-left: 32.5px;
+text-align: left;
+
+/* Subtitle */
+font-family: Inter;
+font-size: 24px;
+font-style: normal;
+font-weight: 500;
+line-height: 121.095%; /* 29.063px */
+` 
+;
+
+export const Picture = styled.img`
+width: 98px;
+height: 98px;
+border-radius: 8px;
+border: 1px solid #DFDFDF;
+margin-left: 30.5px;
+display: flex;
+
+`;
+export const SubjectTitle = styled.h2`
+width: 129px;
+height: 22px;
+font-family: Inter;
+font-size: 14px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+color: #000;
+display: flex;
+gap: 10px;
+
+
+`;
+export const SubjectDescription = styled.p`
+overflow: hidden;
+color: #8A8A8A;
+font-family: Inter;
+font-size: 12px;
+font-style: normal;
+line-height: normal;
+display: flex;
 `;
 
-export const SubjectCard = styled.div`
-  background-color: white;
-  border-radius: 12px;
-  padding: 20px;
-  width: 200px;
-  height: 120px;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-  font-weight: bold;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+export const SubjectBlock = styled.div`
+display: flex;
+gap: 16px;
+padding-bottom: 16px;
+margin-bottom: 16px;
+border-bottom: 1px solid #E0E0E0
 `;
-
-export default HomePageStyle;
+ 
 
